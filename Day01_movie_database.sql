0498e0a2925f37b358feeb0ed62b3750a308cb8a
Paras Guglani <30524022+parasguglani@users.noreply.github.com>
2026-06-14 23:17:11 +0530

    Update Day01_movie_database.sql

--- Day01_movie_database.sql
+++ Day01_movie_database.sql
@@ -3,4 +3,6 @@ use moviestore;
 create table movie (
 id INT Primary Key, Movie_name varchar(50), 
 Hero varchar(50), release_year int, rating INT);
-
+insert into movie values (1, 'Jawan', 'Shah Rukh Khan', 2023,8),
+(2, 'Pushpa', 'Allu Arjun', 2021, 8), (3, 'KGF2', 'yash', 2022,9),
+(4,  'Leo', 'Vijay', 2023,7), (5, 'Kalki 2898 AD', 'Prabhas',2024, 9);
