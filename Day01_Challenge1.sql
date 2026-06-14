e6e63cf007744e2e1187bc2a698aa01d4fabea75
Paras Guglani <30524022+parasguglani@users.noreply.github.com>
2026-06-15 01:47:41 +0530

    Create Day01_Challenge1.sql
    create employee database and write queries 
    Show all employees.
    Show only emp_name.
    Show emp_name and salary.
    Show employees whose salary is greater than 50000.
    Show employees from 'Delhi'

---
+++ Day01_Challenge1.sql
@@ -0,0 +1,15 @@
+/* Create database table employee 
+Column	Type
+emp_id	INT
+emp_name	VARCHAR(50)
+department	VARCHAR(50)
+salary	INT
+city	VARCHAR(50)
+                               Insert 7 employees
+Then write queries for:
+Show all employees.
+Show only emp_name.
+Show emp_name and salary.
+Show employees whose salary is greater than 50000.
+Show employees from 'Delhi'.
+*/
