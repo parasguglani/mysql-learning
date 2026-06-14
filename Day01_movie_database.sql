bdf5b127b323bec605f335ab59a2375b4886e698
Paras Guglani <30524022+parasguglani@users.noreply.github.com>
2026-06-14 23:23:16 +0530

    Update Day01_movie_database.sql

--- Day01_movie_database.sql
+++ Day01_movie_database.sql
@@ -6,7 +6,7 @@ Hero varchar(50), release_year int, rating INT);
 insert into movie values (1, 'Jawan', 'Shah Rukh Khan', 2023,8),
 (2, 'Pushpa', 'Allu Arjun', 2021, 8), (3, 'KGF2', 'yash', 2022,9),
 (4,  'Leo', 'Vijay', 2023,7), (5, 'Kalki 2898 AD', 'Prabhas',2024, 9);
-select * from movie; 
-select Movie_name from movie;
-select hero, rating from movie;
-select Movie_name, release_year from movie;
+select * from movie;  /* show full table*/
+select Movie_name from movie; /* show only movie_name table*/
+select hero, rating from movie; /* show only hero and rating table*/
+select Movie_name, release_year from movie; /* show only Movie_name and release year table*/
