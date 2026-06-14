b0f64d6d6f59eafdc429f6ccb1caa54ac3f4fd10
Paras Guglani <30524022+parasguglani@users.noreply.github.com>
2026-06-14 22:40:01 +0530

    Update Day01_Create_Database.sql

--- Day01_Create_Database.sql
+++ Day01_Create_Database.sql
@@ -10,3 +10,8 @@ CREATE TABLE empdata (
 
 insert into empdata values (1,'paras', 23, 10000, 'computers');
 select * from empdata;
+
+/* adding multiple data types in to single sql)*/
+
+insert into mobile values (6, 'nothing', 'phone 2a' , 31999, 8),
+(7, 'oneplus', 'CE6', 28999, 8);
